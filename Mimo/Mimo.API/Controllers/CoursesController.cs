9c294ccd6aa22b6b38ad8c785387978e43092f87
LeventeFabry <levente.fabry@matterofform.com>
2025-02-27 16:08:03 +0100

    init

---
+++ Mimo/Mimo.API/Controllers/CoursesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mimo.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CoursesController : ControllerBase
+{
+    [HttpGet(Name = "GetCourses")]
+    public ActionResult GetCourses()
+    {
+        return Ok("Courses Test");
+    }
+}
